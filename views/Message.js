2ef86555e40d439addd2174715514dd4189751ce
Dustin Crosson (drc518) <drc518@mail.usask.ca>
2021-03-28 17:55:02 -0600

    changes from other branch added, can add message to db but still cannot append to an array without overwriting it

--- views/Message.js
+++ views/Message.js
@@ -1,34 +1,94 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert} from 'react-native';
-import MessageScroll from '../components/MessageDisplay/MessageScroll'
+import {StyleSheet, Text, View, Button, Alert, TouchableOpacity} from 'react-native';
+import MessageScroll from '../components/MessageScroll'
+import {auth, db} from "../firebase/Fire";
+import {TextInput} from "react-native-gesture-handler";
 
 class Message extends React.Component{
 
     constructor(props) {
         super(props)
         this.state = {
-            chatID: this.props.route.params.roomID,
+            chatID: "g84iVN3pgrI0czGiTtAA",
+            message: ''
         }
     }
 
+    sendMessage = async e => {
+        e.preventDefault();
+        let date = new Date();
+
+        // create a new message document
+        // todo: update time stamp to SERVER TIME, couldn't get it working, moved on instead
+        const res = await db.collection('messages').add({
+            msg: this.state.message,
+            timeSent: date.getDate()+ "/"+date.getMonth() + "/" + date.getFullYear(),
+            uid: auth.currentUser.uid
+        });
+
+        const res2 = await db.collection('indivualChats').doc(this.state.chatID).update({
+            messages: db.FieldValue.arrayUnion({
+                mid: res.id,
+                msg: this.state.message,
+                timeSent: date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear(),
+                uid: auth.currentUser.uid})
+        });
+        window.alert("completed 2");
+
+        this.setState({
+            message: '',
+        })
+
+
+    };
 
     render() {
-    return (
-        <View style={styles.container}>
-            <MessageScroll chatID={this.state.chatID}></MessageScroll>
-        </View>
+
+        return (
+            <View style={styles.container}>
+                <MessageScroll chatID={this.state.chatID}/>
+
+                <TextInput
+                    style={styles.input}
+                    value={this.state.message}
+                    onChangeText={message => this.setState({message})}
+                    placeholder='message to send'
+                    autoCapitalize='none'
+                />
+
+                <TouchableOpacity style={styles.button} onPress={this.sendMessage}>
+                    <Text style={styles.buttonText}>Send Message</Text>
+                </TouchableOpacity>
+
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-      backgroundColor: 'white',
+        backgroundColor: 'white',
     },
     text: {
-      fontSize: 50,
-      color: 'blue'
+        fontSize: 50,
+        color: 'blue'
+    },
+    input: {
+        bottom: 0,
+        fontSize: 50,
+        color: 'blue'
+    },
+    buttonText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'black'
+    },
+    button: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#fff',
+        borderWidth: 5
     }
-  });;
+});
 
 export default Message;
