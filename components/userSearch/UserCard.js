ebe9cbf8c71ddf1ad59761d5639ab3c1d141dcef
Dustin Crosson (drc518) <drc518@mail.usask.ca>
2021-04-05 20:05:06 -0600

    when a user is clicked on, a new chatroom is created

--- components/userSearch/UserCard.js
+++ components/userSearch/UserCard.js
@@ -3,6 +3,7 @@ import {View, StyleSheet, Dimensions, Text} from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { cos } from 'react-native-reanimated';
 import Context from '../../contextAPI/context';
+import { Parse } from "parse/react-native";
 
 
 
@@ -10,38 +11,48 @@ export default class UserCard extends React.Component {
     static contextType = Context
 
     onNewUserPressed = async() => {
+        let currentUserID = await Parse.User.current().id;
+        let userClickedOn = this.props.uid;
         const data = {
             messages: [],
-            userIDs: [this.context.userData.uid, this.props.uid]
-        }
-        const result = await db.collection('indivualChats').add(data);
-
-
-
-        const selfRef = db.collection('users').doc(this.context.userData.uid)
-        const selfData = (await selfRef.get()).data()
-        if('messageRooms' in selfData) {
-            const data = selfData.messageRooms
-            data.push(result.id)
-            await selfRef.update({messageRooms: data})
-        } else {
-            await selfRef.update({messageRooms: [result.id]})
+            userIDs: [currentUserID, userClickedOn]
         }
 
+        //create new chatroom and save my userName + the clicked on persons username
+        const ChatRoom = Parse.Object.extend("ChatRooms");
+        const chatRoom = new ChatRoom();
+        chatRoom.set('users', [[currentUserID, userClickedOn]])
+        const result = await chatRoom.save();
 
-        const otherUserRef = db.collection('users').doc(this.props.uid)
-        const otherUserData = (await otherUserRef.get()).data()
-        if('messageRooms' in otherUserData){
-            const data = otherUserData.messageRooms
-            data.push(result.id)
-            await otherUserRef.update({messageRooms: data})
-        }
+        const chatRoomID = result.id;
+        //add message room to my user
 
-        else {
-            await otherUserRef.update({messageRooms: [result.id]})
-        }
+        //add message room to the new user
 
-        this.props.navigation.navigate('Message', {roomID: result.id})
+        // const selfRef = db.collection('users').doc(this.context.userData.uid)
+        // const selfData = (await selfRef.get()).data()
+        // if('messageRooms' in selfData) {
+        //     const data = selfData.messageRooms
+        //     data.push(result.id)
+        //     await selfRef.update({messageRooms: data})
+        // } else {
+        //     await selfRef.update({messageRooms: [result.id]})
+        // }
+        //
+        //
+        // const otherUserRef = db.collection('users').doc(this.props.uid)
+        // const otherUserData = (await otherUserRef.get()).data()
+        // if('messageRooms' in otherUserData){
+        //     const data = otherUserData.messageRooms
+        //     data.push(result.id)
+        //     await otherUserRef.update({messageRooms: data})
+        // }
+        //
+        // else {
+        //     await otherUserRef.update({messageRooms: [result.id]})
+        // }
+        //
+        // this.props.navigation.navigate('Message', {roomID: result.id})
     }
 
     render(){
